25fe94966b2ae380f16d16059b547e3209bbdf58
Hyevvy <hyeg0@naver.com>
2022-01-13 19:48:05 +0900

    :recycle: refactor: BannerList코드 정리

--- src/components/BannerList.js
+++ src/components/BannerList.js
@@ -1,8 +1,33 @@
-import MySwiperList from "./MySwiperList";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
 const BannerList = ({ list }) => {
   return (
-    <div className="DanceAcademies">
-      <MySwiperList list={list} />
+    <div className="MySwiperList">
+      <Swiper
+        className="MySwiperList__swiper"
+        spaceBetween={30}
+        slidesPerView={4}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {list.map((it) => (
+          <SwiperSlide key={it.id} className="MySwiperList__slideItem">
+            <img
+              width="130px"
+              src={process.env.PUBLIC_URL + `assets/danceacademy_logo.png`}
+            />
+            <div className="MySwiperList__rate">⭐️{it.rate}</div>
+            <div className="MySwiperList__name">{it.name}</div>
+            <div className="MySwiperList__description">{it.location}</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
