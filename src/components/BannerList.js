f6e8a07797ca90b93df1b684cbc0b7bd83c560be
Hyevvy <hyeg0@naver.com>
2022-01-26 19:44:27 +0900

    :sparkles: feat : 댄스학원, 연습실정보 서버통신 구현

--- src/components/BannerList.js
+++ src/components/BannerList.js
@@ -3,12 +3,13 @@ import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
-const BannerList = ({ text,  list }) => {
+const BannerList = ({ text, list }) => {
   const navigate = useNavigate();
+
   return (
     <div className="MySwiperList">
       <h4>{text}</h4>
@@ -21,13 +22,14 @@ const BannerList = ({ text,  list }) => {
         pagination={{ clickable: true }}
       >
         {list.map((it) => (
-          <SwiperSlide key={it.id} className="MySwiperList__slideItem"
-            onClick={()=>{navigate('/detail/3')}}
+          <SwiperSlide
+            key={it.id}
+            className="MySwiperList__slideItem"
+            onClick={() => {
+              navigate(`/detail/${it.id}`);
+            }}
           >
-            <img
-              width="130px"
-              src={process.env.PUBLIC_URL + `assets/danceacademy_logo.png`}
-            />
+            <img width="130px" src={it.picture_url} />
             <div className="MySwiperList__rate">⭐️{it.rate}</div>
             <div className="MySwiperList__name">{it.name}</div>
             <div className="MySwiperList__description">{it.location}</div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import { useNavigate } from "react-router-dom";
-import {useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import MyHeader from "../components/MyHeader";
 import MyFooter from "../components/MyFooter";
@@ -10,120 +10,78 @@ import About from "../components/About";
 import BannerList from "../components/BannerList";
 import { LoginDispatchContext } from "../App";
 import { LoginContext } from "../App";
+import axios from "axios";
+import API from "../shared/Request";
+
 const Home = () => {
+  const getMainInfoAcademy = async () => {
+    try {
+      const res = await API.get("/api/main/GetMainInfo/Academy");
+      const _inputData = await res.data.data.map((rowData) => ({
+        id: rowData.POST_NO,
+        name: rowData.POST_NAME,
+        picture_url: rowData.POST_PICTURE,
+        rate: rowData.POST_RATE,
+        location: rowData.POST_LOCATION,
+      }));
+      setAcademies(academies.concat(_inputData));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getMainInfoDanceRooms = async () => {
+    try {
+      const res = await API.get("/api/main/GetMainInfo/PracticeRoom");
+      console.log(res.data);
+      const _inputData = await res.data.data.map((rowData) => ({
+        id: rowData.POST_NO,
+        name: rowData.POST_NAME,
+        picture_url: rowData.POST_PICTURE,
+        rate: rowData.POST_RATE,
+        location: rowData.POST_LOCATION,
+      }));
+      setDanceRooms(dancerooms.concat(_inputData));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getMainInfoDancers = async () => {
+    try {
+      const res = await API.get("/api/main/GetMainInfo/Dancer");
+      console.log(res);
+      const _inputData = await res.data.data.map((rowData) => ({
+        id: rowData.POST_NO,
+        name: rowData.POST_NAME,
+        picture_url: rowData.POST_PICTURE,
+        rate: rowData.POST_RATE,
+        genre: rowData.POST_JANRE,
+      }));
+      setDancers(dancers.concat(_inputData));
+    } catch (e) {
+      console.log(e);
+    }
+  };
   const navigate = useNavigate();
   const login = useContext(LoginContext);
-  const toggleLogin = useContext(LoginDispatchContext)
+  const toggleLogin = useContext(LoginDispatchContext);
   const [isHamPushed, setIsHamPushed] = useState(false);
 
+  const [academies, setAcademies] = useState([]);
+  const [dancerooms, setDanceRooms] = useState([]);
+  const [dancers, setDancers] = useState([]);
+
   useEffect(() => {
+    getMainInfoAcademy();
+    getMainInfoDanceRooms();
+    getMainInfoDancers();
     console.log(login);
   }, []);
-
-
-  const dancerooms = [
-    {
-      id: 1,
-      rate: 4.5,
-      name: "댄스 연습실 1",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 2,
-      rate: 4.5,
-      name: "댄스 연습실 2",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 3,
-      rate: 4.5,
-      name: "댄스 연습실 3",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 4,
-      rate: 4.5,
-      name: "댄스 연습실 4",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 5,
-      rate: 4.5,
-      name: "댄스 연습실 5",
-      location: "서울특별시 강남구 논현로1길",
-    },
-  ];
-
-  const academies = [
-    {
-      id: 1,
-      rate: 4.5,
-      name: "댄스 스튜디오1",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 2,
-      rate: 4.0,
-      name: "댄스 스튜디오2",
-      location: "서울특별시 강남구 논현로2길",
-    },
-    {
-      id: 3,
-      rate: 3.5,
-      name: "댄스 스튜디오3",
-      location: "서울특별시 강남구 논현로3길",
-    },
-    {
-      id: 4,
-      rate: 3.0,
-      name: "댄스 스튜디오4",
-      location: "서울특별시 강남구 논현로4길",
-    },
-    {
-      id: 5,
-      rate: 3.5,
-      name: "댄스 스튜디오5",
-      location: "서울특별시 강남구 논현로5길",
-    },
-  ];
-
-  const dancers = [
-    {
-      id: 1,
-      rate: 4.5,
-      name: "댄서1",
-      location: "서울특별시 강남구 논현로1길",
-    },
-    {
-      id: 2,
-      rate: 4.0,
-      name: "댄서2",
-      location: "서울특별시 강남구 논현로2길",
-    },
-    {
-      id: 3,
-      rate: 3.5,
-      name: "댄서3",
-      location: "서울특별시 강남구 논현로3길",
-    },
-    {
-      id: 4,
-      rate: 3.0,
-      name: "댄서4",
-      location: "서울특별시 강남구 논현로4길",
-    },
-    {
-      id: 5,
-      rate: 3.5,
-      name: "댄서",
-      location: "서울특별시 강남구 논현로5길",
-    },
-  ];
 
   return (
     <div className="Home">
       <MyHeader
-        
         rightChild={
           login ? (
             <GiHamburgerMenu
@@ -141,7 +99,7 @@ const Home = () => {
           <div
             className="Home__innerhamburger__item"
             onClick={() => {
-              navigate('/mypage/3');
+              navigate("/mypage/3");
             }}
           >
             {" "}
@@ -162,18 +120,9 @@ const Home = () => {
       )}
 
       <MyBanner />
-      <BannerList
-        text={"지금! 뜨고 있는 댄스 학원"}
-        list={academies}
-      />
-      <BannerList
-        text={"요즘 인기 있는 연습실"}
-        list={dancerooms}
-      />
-      <BannerList
-        text={"지금 HOT한 댄서는 누구?"}
-        list={dancers}
-      />
+      <BannerList text={"지금! 뜨고 있는 댄스 학원"} list={academies} />
+      <BannerList text={"요즘 인기 있는 연습실"} list={dancerooms} />
+      <BannerList text={"지금 HOT한 댄서는 누구?"} list={dancers} />
       <h4>ReadyDance에서 댄스에 대한 모든 것을 알아보세요!</h4>
       <section className="Home__about">
         <About headText="레디댄스란 무엇인가요?" />
