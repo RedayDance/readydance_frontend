18dda768d9b2691f715034cd3a231c25af9cdd77
Hyevvy <hyeg0@naver.com>
2022-01-30 19:16:33 +0900

    :sparkles: feat : 마이페이지 개인정보 페이지 구현

--- src/components/MyPagePi.js
+++ src/components/MyPagePi.js
@@ -1,7 +1,27 @@
+import MyButton from "./MyButton";
+
 const MyPagePi = () =>{
     return(
         <div className="MyPagePi">
-            개인정보!
+            <div className="MyPagePi__innerDiv">
+                <div>아이디</div>
+                <div className="MyPagePi__innerDiv__userInput">asdf@naver.com</div>
+            </div>
+
+            <div className="MyPagePi__innerDiv">
+                <div>닉네임</div>
+                <div className="MyPagePi__innerDiv__userInput">hyevvy</div>
+            </div>
+
+            <div className="MyPagePi__innerDiv">
+                <div>휴대폰 번호</div>
+                <div className="MyPagePi__innerDiv__userInput">010-1234-5678</div>
+            </div>
+
+            <div className="MyBtn"> 
+            <MyButton  text="저장 완료" onClick={()=>{alert("정보를 수정합니다")}} />
+            </div>
+          
         </div>
     )
 }
